03819df9816e18abe32099b88553fe3624e1a6bc
Radek Benes <radekbenes@centrum.cz>
2016-03-11 17:02:33 +0100

    11-03-16

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
+//Project overlay initialization
+$(function () {
+    Boxgrid.init();
+});
+
+$(window).resize(function () {
+    ProjectBottomResize();
+})
+
 $(document).ready(function () {
     //Parallax
     parallaxInit();
+
     function parallaxInit() {
         $('header').parallax("30%", 0.1);
         $('#stripe-banner').parallax("30%", 0.1);
@@ -36,3 +46,16 @@ $(document).ready(function () {
     //Carousel
     $('.carousel').carousel();
 });
+
+//Resize .project-bottom height
+function ProjectBottomResize () {
+    var windowHeight = $(window).height();
+    var itemHeight = $('.project-bottom').height();
+    var ratio = itemHeight / windowHeight * 100
+    var bottomShift = (ratio - 40) * 2;
+
+    if (ratio > 40) {
+        $('.project-bottom').css('bottom', -bottomShift + '%')
+    }
+}
+
