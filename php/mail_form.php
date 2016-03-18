99ba9525691494425331d46ae1bfac516f1340ed
Radek Benes <radekbenes@centrum.cz>
2016-03-18 10:04:09 +0100

    18-03-16

--- php/mail_form.php
+++ php/mail_form.php
@@ -1,22 +1,18 @@
-<?
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-//$thanksPage = 'http://www.divadlovetry.cz/thanks.html';
-//$nothanksPage = 'http://www.divadlovetry.cz/nothanks.html';
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Portfolio page';
+    $to = 'radeg007@gmail.com';
+    $subject = 'Hello';
 
-if ($email!="" and $message!="" and $name!="")
-{
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-$site_name = "Portfolio";
-$header   = "From: ". $name . " <" . $email . ">\r\n";
-$header   .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-
-mail("radeg007@gmail.com", $message, "From: " . $email, $site_name);
-echo '<p>Your message has been sent!</p>';
-}
-else
-{
-echo '<p>Something went wrong, go back and try again!</p>';
-}
+    if ($_POST['submit'] && $human == '4') {
+        if (mail ($to, $subject, $body, $from)) {
+	    echo '<p>Your message has been sent!</p>';
+	} else {
+	    echo '<p>Something went wrong, go back and try again!</p>';
+	}
+    }
 ?>
